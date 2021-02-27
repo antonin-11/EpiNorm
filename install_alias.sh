3d6295532b325e80834b308dcf183b52d96f6b1a
Nathan <nathan.guiu@epitech.eu>
2021-02-27 19:15:10 +0100

    fix install alias for linux

--- install_alias.sh
+++ install_alias.sh
@@ -3,13 +3,13 @@ env_term=$(printenv SHELL)
 if [ $env_term = "/bin/zsh" ]
 then
         chmod 777 ~/.zshrc
-        echo "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.zshrc
+        echo -ne "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.zshrc
         chmod 644 ~/.zshrc
-        echo "\033[1m\033[32msuccessful alias installation\033[0m"
+        echo -ne "\033[1m\033[32msuccessful alias installation\033[0m\n"
 elif [ $env_term = "/bin/bash" ]
 then
-        echo "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.bashrc
-        echo "\033[1m\033[32msuccessful alias installation\033[0m"
+        echo -ne "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.bashrc
+        echo -ne "\033[1m\033[32msuccessful alias installation\033[0m\n"
 else
-        echo "\033[1m\033[31mError install alias\033[0m"
+        echo -ne "\033[1m\033[31mError install alias\033[0m\n"
 fi
