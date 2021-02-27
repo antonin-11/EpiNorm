ae45a149073f54adb9341b215aa1efee03db9a32
antonin-11 <antonin.ricard@epitech.eu>
2021-02-27 19:23:54 +0100

    corection install for linux

--- install_alias.sh
+++ install_alias.sh
@@ -3,9 +3,9 @@ env_term=$(printenv SHELL)
 if [ $env_term = "/bin/zsh" ]
 then
         chmod 777 ~/.zshrc
-        echo -ne "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.zshrc
+        echo "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.zshrc
         chmod 644 ~/.zshrc
-        echo -ne "\033[1m\033[32msuccessful alias installation\033[0m\n"
+        echo "\033[1m\033[32msuccessful alias installation\033[0m"
 elif [ $env_term = "/bin/bash" ]
 then
         echo -ne "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.bashrc
