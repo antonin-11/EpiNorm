b1a3bff413608398065b4dc9d400c049df0797cd
antonin-11 <71838614+antonin-11@users.noreply.github.com>
2021-03-23 21:44:03 +0100

    bug fixing, fixing norm for .h, fixing error norm

--- EpiNorm.py
+++ EpiNorm.py
@@ -1,6 +1,5 @@
 #!/bin/env python3
-
-from os import walk
+import os
 
 def check_o2(name, file):
     if (name[-2:] != ".h" and name[-2:] != ".c"):
@@ -8,7 +7,13 @@ def check_o2(name, file):
 
 
 def verif_norm(path, file_name):
-    if (file_name[-2:] == ".h" or file_name[-2:] == ".c"):
+    if (file_name[-2:] == ".h"):
+        fd = open(path, "r")
+        sto = fd.read()
+        file = sto.split('\n')
+        check_v1(path, file)
+        fd.close()
+    if (file_name[-2:] == ".c"):
         fd = open(path, "r")
         sto = fd.read()
         file = sto.split('\n')
@@ -16,7 +21,7 @@ def verif_norm(path, file_name):
         check_f4(path, file)
         check_O3(path, file)
         check_L6(path, file)
-        check_V1(path, file)
+        check_V1_file(path, file)
         check_C1(path, file)
         check_G2(path, file)
         check_G1(path, file)
@@ -27,10 +32,11 @@ def verif_norm(path, file_name):
         end_space(path, file)
         fd.close()
     else:
-        print_error(path, 0, "Type de fichier interdit.", "green")
+        if (file_name != "Makefile"):
+            print_error(path, 0, "Type de fichier interdit.", "green")
 
 def main():
-    for (repertoire, sousRepertoires, fichiers) in walk("./"):
+    for (repertoire, sousRepertoires, fichiers) in os.walk("./"):
         no_verif = [".git", ".gitignore", "tests"]
         verif = True
         for name in no_verif:
@@ -61,6 +67,21 @@ def print_error(file, line, info, color):
 
 # ------CHECK------ #
 
+def check_V1(name, file):
+    declaration = ["int ", "char ", "float ", "bool ", "_t"]
+    num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for index, line in enumerate(file):
+        for p in range(5):
+            for q in range(10):
+                if (declaration[p] in line and num[q] in line and "//" not in line):
+                    if p == 5:
+                        print_error(name, index + 1, "In your declaration of struct you have a number (" + num[q] + ")." , "yellow")
+                    else:
+                        print_error(name, index + 1, "In your declaration [" + declaration[p] + "] you have a number (" + num[q] + ")." , "yellow")
+    for index, line in enumerate(file):
+        if ("typedef" in line and "_t" not in line):
+            print_error(name, index + 1, "In your declaration typedef[struct] you have a invalid name (need *_t)." , "red")
+
 def check_f4(name, file):
     max_len = len(file)
     for i in range(len(file)):
@@ -82,7 +103,7 @@ def check_O3(name, file):
         if file[i][:1] == '{':
             fonction += 1
     if fonction > 5:
-        print_error(name, 0, "Plus de 5 fonctions dans le fichier (" + str(fonction) + " > 5).", "red")
+        print_error(name, 0, "More than 5 functions in the file (" + str(fonction) + " > 5).", "red")
 
 def check_L6(name, file):
     conteur = 0
@@ -96,11 +117,11 @@ def check_L6(name, file):
             if espace == 0:
                 espace += 1
             else:
-                print_error(name, (i+1), "Plus de 1 retour à ligne d'affilé.", "yellow")
+                print_error(name, (i+1), "More than 1 return line in a row.", "yellow")
         else:
             espace = 0
 
-def check_V1(name, file):
+def check_V1_file(name, file):
     fault = 0
     for x in name.split('/')[-1][:-2]:
         if (x.isalnum()):
@@ -109,21 +130,21 @@ def check_V1(name, file):
         elif (x != '_'):
             fault = 1
     if (fault == 1):
-        print_error(name, 0, "Le nom du fichier du fichier doit respecter la snake case convention.", "red")
+        print_error(name, 0, "The filename of the file must follow the snake case convention.", "red")
 
 def check_C1(name, file):
     error = 0
     for index, line in enumerate(file):
         space = 0
         if (len(line) > 0):
-            while (line[space] == ' '):
+            while (space < len(line) and line[space] == ' '):
                 space += 1
             if (space > 12 and error == 0):
                 error = 1
-                print_error(name, index, "Trop de condition ou de boucle imbriquée.", "yellow")
-            elif (space == 8 and "else if" in line):
+                print_error(name, index, "Too many condition or nested loop.", "yellow")
+            elif (space >= 8 and "else if" in line):
                 error = 1
-                print_error(name, index + 1, "Trop de condition ou de boucle imbriquée.", "yellow")
+                print_error(name, index + 1, "Too many condition or nested loop.", "yellow")
             else:
                 error = 0
 
@@ -132,14 +153,14 @@ def check_G2(name, file):
         if (i == 0):
             i += 1
         if (file[i - 1][:1] == "}" and file[i][:1].isalnum() == True):
-            print_error(name, i, "Il manque un espace entre 2 fonctions.", "yellow")
+            print_error(name, i, "You forgot a space between 2 functions.", "yellow")
 
 def check_G1(name, file):
     if (len(file) < 7):
-        print_error(name, 0, "le fichier est vide ou le headeur n'est pas présent.", "yellow")
+        print_error(name, 0, "The file is empty or the header is not present.", "yellow")
     for i in range(6):
         if (file[i][:2] != "/*" and file[i][:2] != "**" and file[i][:2] != "*/"):
-            print_error(name, 0, "le heauder est absent ou présente un problème.", "yellow")
+            print_error(name, 0, "The header is missing or has a problem.", "yellow")
 
 def check_f6(name, file):
     bracket = 0
@@ -161,7 +182,7 @@ def check_f6(name, file):
                 slash_etoile = 0
                 slash = 0
             if (slash == 2 or slash_etoile == 2):
-                print_error(name, index + 1, "Un commentaire est présent dans une fonction.", "yellow")
+                print_error(name, index + 1, "A comment is present in a function.", "yellow")
 
 def check_L2(name, file):
     space = 0
@@ -170,20 +191,21 @@ def check_L2(name, file):
         while space < len(file[i]) and file[i][space] == ' ':
             space += 1
         if (space != 0 and space != 4 and space != 8 and space != 12 and space != 16):
-            print_error(name, i + 1, "Toutes les indentations doivent être composée de 4 espaces.", "yellow")
+            print_error(name, i + 1, "All indentations must be made up of 4 spaces.", "yellow")
     for i in range(len(file)):
         tab = 0
         for y in range(len(file[i])):
             if file[i][y] == '\t':
-                print_error(name, i + 1, "les tabulations sont interdites et doive être remplacé par 4 espaces.", "yellow")
+                print_error(name, i + 1, "Tabulations are prohibited and must be replaced by 4 spaces.", "yellow")
 
 def check_L3(name, file):
-    mot_cle = ["for", "while", "if", "else", "else if", "return"]
-    mot_cle2 = ["for ", "while ", "if ", "else ", "else if ", "return "]
+    mot_cle = [" for(", " while(", " if(", " else(", " else if(", " return("]
+    mot_cle2 = [" for ", " while ", " if ", " else", " else if ", " return "]
+    mot_cle3 = ["for", "while", "if", "else", "else if", "return"]
     for index, line in enumerate(file):
         for p in range(5):
-            if (mot_cle[p] in line and mot_cle2[p] not in line):
-                print_error(name, index + 1, "Il manque un espace après un [" + mot_cle[p] + "]." , "yellow")
+            if (mot_cle[p] in line):
+                print_error(name, index + 1, "A space is missing after a [" + mot_cle3[p] + "]." , "yellow")
 
 def check_L4(name, file):
     prochain = 0
@@ -197,16 +219,16 @@ def check_L4(name, file):
                     bracket = 1
                 i += 1
             if bracket == 1:
-                print_error(name, index, "Erreur de curly brackets" , "yellow")
+                print_error(name, index, "Error in the order of curly brackets." , "yellow")
             bracket = 0
             prochain = 0
-        if 'if' in line or 'while' in line or 'for' in line:
+        if ' if ' in line or ' while ' in line or ' for ' in line or ' else if ' in line:
             prochain = 1
 
 def end_space(name, file):
     for index, line in enumerate(file):
         reverse = line[::-1]
         if (len(reverse) > 0 and reverse[0] == ' '):
-            print_error(name, index + 1, "Espace(s) en fin de ligne" , "green")
+            print_error(name, index + 1, "You left a space at the end of the line." , "green")
 
 main()
--- install_alias.sh
+++ install_alias.sh
@@ -1,26 +1,58 @@
 env_term=$(printenv SHELL)
 
+if [[ $1 = "--update" ]]
+then
+        cd $2
+        update=$(git diff)
+        if [[ $update != "" ]]
+        then
+                # git pull > dev/null 2 > &1
+                echo "\033[1m\033[32mA new update has been installed\033[0m"
+        fi
+        exit
+fi
+
 if [ $env_term = "/bin/zsh" ]
 then
         file=$(cat ~/.zshrc)
         if [[ $file =~ "epinorm" ]]
         then
-                echo "\033[1m\033[31mthe alias is already installed\033[0m"
+                echo "\033[1m\033[31mThe alias is already installed\033[0m"
         else
                 chmod 777 ~/.zshrc
-                echo "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.zshrc
+                echo "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.zshrc
                 chmod 644 ~/.zshrc
+                source ~/.zshrc
                 echo "\033[1m\033[32msuccessful alias installation\033[0m"
+        fi
+        if [[ $file =~ "up_epinorm" ]]
+        then
+                echo "\033[1m\033[31mAutomatic updates are already installed\033[0m"
+        else
+                chmod 777 ~/.zshrc
+                echo "\nalias up_epinorm=\"$PWD/install_alias.sh --update\"" >> ~/.zshrc
+                chmod 644 ~/.zshrc
+                source ~/.zshrc
+                echo "\033[1m\033[32mAutomatic updates are installed\033[0m"
         fi
 elif [ $env_term = "/bin/bash" ]
 then
         file=$(cat ~/.bashrc)
         if [[ $file =~ "epinorm" ]]
         then
-                echo -ne "\033[1m\033[31mthe alias is already installed\033[0m"
+                echo -ne "\033[1m\033[31mThe alias is already installed\033[0m"
         else
-                echo -ne "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"\nalias Epinorm=\"python3 $PWD/EpiNorm.py\"\nalias EpiNorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.bashrc
+                echo -ne "\n\nalias epinorm=\"python3 $PWD/EpiNorm.py\"" >> ~/.bashrc
+                source ~/.bashrc
                 echo -ne "\033[1m\033[32msuccessful alias installation\033[0m\n"
+        fi
+        if [[ $file =~ "up_epinorm" ]]
+        then
+                echo -ne "\033[1m\033[31mAutomatic updates are already installed\033[0m"
+        else
+                echo -ne "\nalias up_epinorm=\"$PWD/install_alias.sh --update\"" >> ~/.bashrc
+                source ~/.bashrc
+                echo -ne "\033[1m\033[32mAutomatic updates are installed\033[0m"
         fi
 else
         echo -ne "\033[1m\033[31mError install alias\033[0m\n"
